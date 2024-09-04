a7f95489383bd05d398b0e52fa5c57ef1baeb2c1
Shani Amos <shaniamos@aol.com>
2024-09-03 20:36:18 -0400

    Lesson 02 code submission

---
+++ python-lesson-02/lesson_02.py
@@ -0,0 +1,10 @@
+text = 'I like candybars! '
+text_echo = text * 10
+print(text_echo)
+first_name = 'Shani'
+print(first_name)
+first_name_upper = first_name.upper()
+print(first_name_upper)
+print(f"My annoying text is {text_echo}")
+print(len(text_echo))
+print(f"It has 180 characters")
